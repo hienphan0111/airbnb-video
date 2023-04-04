5dbf8aa267a76ad7317b2f2cf011c7c8d9f7e11c
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-04-04 18:39:20 +0700

    Add navbar

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,18 @@
+import Container from '../Container';
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+        py-4
+        border-b-[1px]">
+        <Container>
+          anything
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
